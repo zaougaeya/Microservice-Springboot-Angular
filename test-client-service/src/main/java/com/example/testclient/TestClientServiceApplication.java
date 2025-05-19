822e4fada8cf49c038726ed47539ca51e65fb909
OumaymaBoughanmi <130233973+OumaymaBoughanmi@users.noreply.github.com>
2025-05-19 01:21:01 +0100

    test client service
    This will allow you to test the user endpoint and will give you an example of how to implement it

---
+++ test-client-service/src/main/java/com/example/testclient/TestClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.testclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.example.testclient.client")
+public class TestClientServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestClientServiceApplication.class, args);
+    }
+}
---
+++ test-client-service/src/main/java/com/example/testclient/model/Role.java
@@ -0,0 +1,6 @@
+package com.example.testclient.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
