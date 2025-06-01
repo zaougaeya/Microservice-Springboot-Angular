a92f5ff929e39e8d6535ac5a66c92f1a04d3d7dd
OumaymaBoughanmi <130233973+OumaymaBoughanmi@users.noreply.github.com>
2025-06-01 16:00:17 +0100

    cors correction

--- gateway-service/src/main/java/com/example/gatewayservice/config/CorsGlobalConfiguration.java
+++ gateway-service/src/main/java/com/example/gatewayservice/config/CorsGlobalConfiguration.java
@@ -6,19 +6,22 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsGlobalConfiguration {
 
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*"); // or specify: http://localhost:4200
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
         config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(List.of("http://localhost:4200")); // ✅ FIXED
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return new CorsWebFilter(source);
     }
 }
